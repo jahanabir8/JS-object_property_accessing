5a62f634c38503b5924634df85423d411282bb1f
jahanabir8 <abir.opediatech@gmail.com>
2022-06-28 10:21:42 +0600

    first upload

---
+++ object.js
@@ -0,0 +1,50 @@
+var point = {
+    x: 10,
+    y: 20,
+    z: 15
+}
+
+console.log(point.x)
+console.log(point.y)
+console.log(point.x+point.z)
+
+console.log(point['x'])
+console.log(point['y'])
+console.log(point['x']+point['z'])
+
+var show1 = 'x'
+var show2 = 'y'
+console.log(point[show1]+point[show2])
+
+// update valye to object property
+
+var point = {
+    x: 10,
+    y: 20,
+    z: 15
+}
+point.x = 100
+console.log(point)
+
+// adding new property in object
+
+var point = {
+    x: 10,
+    y: 20,
+    z: 15
+}
+
+point.a = 200
+console.log(point)
+
+// delete property in object
+
+var point = {
+    p: 10, 
+    q: 20,
+    r: 30,
+    s: 40
+}
+console.log(point)
+delete point.s
+console.log(point)
